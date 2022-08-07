2717b820195aac3515a652f442e89b1e647a4f6d
ryan20bv <ryan20bv@yahoo.com>
2022-08-07 19:05:51 +0800

    implement lazy loading

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import Home from "./pages/home/Home";
-import About from "./pages/about/About";
-import Project from "./pages/project/Project";
-import MiniApp from "./pages/mini-app/MiniApp";
+// import Home from "./pages/home/Home";
+// import About from "./pages/about/About";
+// import Project from "./pages/project/Project";
+// import MiniApp from "./pages/mini-app/MiniApp";
+const Home = React.lazy(() => import("./pages/home/Home"));
+const About = React.lazy(() => import("./pages/about/About"));
+const Project = React.lazy(() => import("./pages/project/Project"));
+const MiniApp = React.lazy(() => import("./pages/mini-app/MiniApp"));
 
 const App = () => {
 	return (
-		<Routes>
-			<Route path='*' element={<Navigate to='/home' />} />
-			<Route path='/home/*' element={<Home />} exact />
-			<Route path='/about' element={<About />} />
-			<Route path='/project' element={<Project />} />
-			<Route path='/mini-app' element={<MiniApp />} />
-		</Routes>
+		<React.Suspense fallback={<>...</>}>
+			<Routes>
+				<Route path='*' element={<Navigate to='/home' />} />
+				<Route path='/home/*' element={<Home />} exact />
+				<Route path='/about' element={<About />} />
+				<Route path='/project' element={<Project />} />
+				<Route path='/mini-app' element={<MiniApp />} />
+			</Routes>
+		</React.Suspense>
 	);
 };
 
