7cb80b087c5a1ada07f97ff575e1cf38b45ee93a
ryan20bv <ryan20bv@yahoo.com>
2022-08-07 20:53:43 +0800

    set up project css

--- portfolio/src/pages/project/Project.js
+++ portfolio/src/pages/project/Project.js
@@ -1,7 +1,44 @@
 import React from "react";
 
+import DivWrapper from "../../ui/DivWrapper";
+import Nav from "../../ui/Nav";
+
+import classes from "./project.module.css";
 const Project = () => {
-	return <div>Project</div>;
+	const leftComponent = (
+		<main className={classes.project}>
+			<Nav
+				head='Project'
+				// fontFamily='Great Vibes'
+				headColor='red'
+				listColor='red'
+			/>
+		</main>
+	);
+	const rightComponent = (
+		<main className={classes.right}>
+			<div className={classes.container}>
+				<h1>Project 1</h1>
+			</div>
+			<div className={classes.container}>
+				<h1>Project 2</h1>
+			</div>
+			<div className={classes.container}>
+				<h1>Project 3</h1>
+			</div>
+			<div className={classes.container}>
+				<h1>Project 4</h1>
+			</div>
+			<div className={classes.container}>
+				<h1>Project 5</h1>
+			</div>
+		</main>
+	);
+	return (
+		<>
+			<DivWrapper leftContent={leftComponent} rightContent={rightComponent} />
+		</>
+	);
 };
 
 export default Project;
