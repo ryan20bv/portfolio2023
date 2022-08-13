d1b0d0439fc5ef080230fa0da38ecdb2f88c622c
ryan20bv <ryan20bv@yahoo.com>
2022-08-13 08:06:59 +0800

    styling done

--- portfolio/src/pages/mini-app/miniAppPages/NumToWords.js
+++ portfolio/src/pages/mini-app/miniAppPages/NumToWords.js
@@ -2,7 +2,23 @@ import React from "react";
 
 import classes from "./assets/styles/numToWord.module.css";
 const NumToWords = () => {
-	return <div className={classes.num}>NumToWords</div>;
+	return (
+		<div className={classes.num}>
+			<h1>Number to Word</h1>
+			<form action=''>
+				<label htmlFor=''>Input the number here:</label>
+				<input type='text' value='103' />
+				<button>Convert to Word/s</button>
+			</form>
+			<div>
+				<p>
+					One Hundred THousand One Billion Three Hundred Fifty Thousand One
+					Hundred THousand One Billion Three Hundred Fifty Thousand
+				</p>
+			</div>
+			<footer>@2022</footer>
+		</div>
+	);
 };
 
 export default NumToWords;
