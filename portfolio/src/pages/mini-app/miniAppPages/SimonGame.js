57549bb5760b85f3ed3bba00ad255a5419ddc72a
ryan20bv <ryan20bv@yahoo.com>
2022-08-13 21:31:51 +0800

    started styling simon game

--- portfolio/src/pages/mini-app/miniAppPages/SimonGame.js
+++ portfolio/src/pages/mini-app/miniAppPages/SimonGame.js
@@ -1,7 +1,27 @@
 import React from "react";
-
+import classes from "./assets/styles/simon.module.css";
 const SimonGame = () => {
-	return <div>SimonGame</div>;
+	return (
+		<main className={classes.simon}>
+			<section className={classes.first}>
+				<h1>SIMON</h1>
+			</section>
+			<section className={classes.playArea}>
+				<button className={classes.start}>Press To Start</button>
+				<div className={classes.info}>score</div>
+				<div className={classes.playDiv}>
+					<div className={classes.button}></div>
+					<div className={classes.button}></div>
+					<div className={classes.button}></div>
+					<div className={classes.button}></div>
+				</div>
+				<footer>@2022</footer>
+			</section>
+			<section className={classes.last}>
+				<h1>GAME</h1>
+			</section>
+		</main>
+	);
 };
 
 export default SimonGame;
