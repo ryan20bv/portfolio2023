9412a5f87dbfd4d847f473ce06c69477661bc572
ryan20bv <ryan20bv@yahoo.com>
2022-08-20 10:46:41 +0800

    add form and style

--- portfolio/src/pages/mini-app/miniAppPages/Raffle.js
+++ portfolio/src/pages/mini-app/miniAppPages/Raffle.js
@@ -1,7 +1,35 @@
 import React from "react";
 
+import classes from "./assets/styles/raffle.module.css";
 const SliderOne = () => {
-	return <div>Raffle</div>;
+	return (
+		<main className={classes.raffle}>
+			<section className={classes.header}>
+				<div className={classes.wrapper}>
+					<form action='' className={classes.form}>
+						<section className={classes.form_div}>
+							<label htmlFor='number'>Select a number:</label>
+							<div>
+								<input type='text' id='number' />
+								<select name='select_number' id='select_number'>
+									<option value='1'>1</option>
+									<option value='2'>2</option>
+									<option value='3'>3</option>
+									<option value='4'>4</option>
+									<option value='5'>5</option>
+								</select>
+							</div>
+						</section>
+						<section className={classes.form_div}>
+							<label htmlFor='name'>Name:</label>
+							<input type='text' id='name' />
+						</section>
+						<button>Confirm</button>
+					</form>
+				</div>
+			</section>
+		</main>
+	);
 };
 
 export default SliderOne;
