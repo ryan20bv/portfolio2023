9c364046dbb7673b799c1c06df124af6be2a97d9
ryan20bv <ryan20bv@yahoo.com>
2022-08-22 09:21:52 +0800

    todo header syled

--- portfolio/src/pages/mini-app/miniAppPages/TodoList.js
+++ portfolio/src/pages/mini-app/miniAppPages/TodoList.js
@@ -4,7 +4,18 @@ import classes from "./assets/styles/todoList.module.css";
 const SliderOne = () => {
 	return (
 		<main className={classes.raffle}>
-			<section className={classes.header}></section>
+			<section className={classes.header}>
+				<form action=''>
+					<input type='text' placeholder='add todo here' />
+					<button className={classes.form_btn} id='add_btn'>
+						Add
+					</button>
+					<button className={classes.form_btn} id='cancel_btn'>
+						Cancel
+					</button>
+				</form>
+			</section>
+			<section className={classes.content}>content</section>
 		</main>
 	);
 };
