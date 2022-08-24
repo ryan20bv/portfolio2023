79dc7b004738fd98570c29c93e31f02c94146635
ryan20bv <ryan20bv@yahoo.com>
2022-08-24 21:33:32 +0800

    style card

--- portfolio/src/pages/mini-app/miniAppPages/Pizzalated.js
+++ portfolio/src/pages/mini-app/miniAppPages/Pizzalated.js
@@ -8,7 +8,37 @@ const Pizzalated = () => {
 			<header>
 				<PizzaNav classes={classes} />
 			</header>
-			<section>content</section>
+			<section>
+				<div>left</div>
+				<div className={classes.content}>
+					<div className={classes.card}>
+						<img
+							src='https://www.ezcater.com/lunchrush/wp-content/uploads/sites/2/2017/10/shutterstock_623344781.jpg.webp'
+							alt='content 1'
+						/>
+						<div className={classes.info}>
+							<h1>Cheesy</h1>
+							<p>$ 3.15</p>
+						</div>
+						<div>
+							<form action=''>
+								<div>
+									<label htmlFor=''>Qty:</label>
+									<button>-</button>
+									<input type='text' defaultValue='1' max='5' />
+									<button>+</button>
+								</div>
+
+								<button type='submit' className={classes.btn_submit}>
+									Add to Cart
+								</button>
+							</form>
+						</div>
+					</div>
+				</div>
+				<div>right</div>
+			</section>
+			<footer>@2022</footer>
 		</main>
 	);
 };
