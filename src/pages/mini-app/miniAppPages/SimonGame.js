dcc49312b5268c5dc653de60604515971da5265c
ryan20bv <ryan20bv@yahoo.com>
2022-12-02 20:44:19 +0800

    reaalligne simon game

--- src/pages/mini-app/miniAppPages/SimonGame.js
+++ src/pages/mini-app/miniAppPages/SimonGame.js
@@ -31,61 +31,62 @@ const SimonGame = () => {
 	// 	classForDivPlay = `${classes.playDiv} ${classes.computerTurn}`;
 	// }
 	return (
-		<main className={classes.simon}>
-			<section className={classes.first}>
-				<h1>SIMON</h1>
-			</section>
-			<section className={classes.playArea}>
-				<button className={classes.start} onClick={playStartHandler}>
-					{isGameOver && "Game Over"}
-					{!isGameOver && playerTurn && "Player Turn"}
-					{!isGameOver && computerTurn && "Computer Turn"}
-					{!isGameOver && !computerTurn && !playerTurn && "Press To Start"}
-				</button>
-				<div className={classes.wrapper} ref={wrongRef}>
-					<div className={classes.info} ref={infoRef}>
-						<h1>
-							Level <span>{gameLevel}</span>
-						</h1>
+		<>
+			<main className={classes.simon}>
+				<section className={classes.first}>
+					<h1>SIMON</h1>
+				</section>
+				<section className={classes.playArea}>
+					<button className={classes.start} onClick={playStartHandler}>
+						{isGameOver && "Game Over"}
+						{!isGameOver && playerTurn && "Player Turn"}
+						{!isGameOver && computerTurn && "Computer Turn"}
+						{!isGameOver && !computerTurn && !playerTurn && "Press To Start"}
+					</button>
+					<div className={classes.wrapper} ref={wrongRef}>
+						<div className={classes.info} ref={infoRef}>
+							<h1>
+								Level <span>{gameLevel}</span>
+							</h1>
 
-						<p className={`${classes.timer}`} ref={timerRef}>
-							{timerValue}
-						</p>
+							<p className={`${classes.timer}`} ref={timerRef}>
+								{timerValue}
+							</p>
+						</div>
+						<div className={classForDivPlay}>
+							<div
+								className={classes.button}
+								id='green'
+								ref={greenRef}
+								onClick={playerClickHandler}
+							></div>
+							<div
+								className={classes.button}
+								id='red'
+								ref={redRef}
+								onClick={playerClickHandler}
+							></div>
+							<div
+								className={classes.button}
+								id='yellow'
+								ref={yellowRef}
+								onClick={playerClickHandler}
+							></div>
+							<div
+								className={classes.button}
+								id='blue'
+								ref={blueRef}
+								onClick={playerClickHandler}
+							></div>
+						</div>
 					</div>
-					<div className={classForDivPlay}>
-						<div
-							className={classes.button}
-							id='green'
-							ref={greenRef}
-							onClick={playerClickHandler}
-						></div>
-						<div
-							className={classes.button}
-							id='red'
-							ref={redRef}
-							onClick={playerClickHandler}
-						></div>
-						<div
-							className={classes.button}
-							id='yellow'
-							ref={yellowRef}
-							onClick={playerClickHandler}
-						></div>
-						<div
-							className={classes.button}
-							id='blue'
-							ref={blueRef}
-							onClick={playerClickHandler}
-						></div>
-					</div>
-				</div>
-
-				<footer>@2022</footer>
-			</section>
-			<section className={classes.last}>
-				<h1>GAME</h1>
-			</section>
-		</main>
+				</section>
+				<section className={classes.last}>
+					<h1>GAME</h1>
+				</section>
+			</main>
+			<footer>@2022</footer>
+		</>
 	);
 };
 
