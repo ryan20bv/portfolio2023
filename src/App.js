babe6de13e47b57a0f76452380436f49f8d6c64d
ryan20bv <ryan20bv@yahoo.com>
2022-12-14 20:09:35 +0800

    added loading page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 // import About from "./pages/about/About";
 // import Project from "./pages/project/Project";
 // import MiniApp from "./pages/mini-app/MiniApp";
+import Loading from "./pages/loading/Loading";
 const Home = React.lazy(() => import("./pages/home/Home"));
 const About = React.lazy(() => import("./pages/about/About"));
 const Project = React.lazy(() => import("./pages/project/Project"));
@@ -12,13 +13,14 @@ const MiniApp = React.lazy(() => import("./pages/mini-app/MiniApp"));
 
 const App = () => {
 	return (
-		<React.Suspense fallback={<>Loading...</>}>
+		<React.Suspense fallback={<Loading />}>
 			<Routes>
 				<Route path='*' element={<Navigate to='/home' />} />
 				<Route path='/home/*' element={<Home />} exact />
 				<Route path='/about' element={<About />} />
 				<Route path='/project' element={<Project />} />
 				<Route path='/mini-app/*' element={<MiniApp />} />
+				<Route path='/loading' element={<Loading />} />
 			</Routes>
 		</React.Suspense>
 	);
---
+++ src/pages/loading/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from "./loading.module.css";
+const Loading = () => {
+	return (
+		<div className={classes.loading}>
+			<h1>RBV</h1>
+		</div>
+	);
+};
+
+export default Loading;
